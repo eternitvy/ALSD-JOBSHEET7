8a649ff6dbd7649dd7268fdc2f7835642ef418ea
aisyaaswy <aisyaidha@gmail.com>
2025-04-21 19:47:15 +0700

    update

--- MataKuliah.java
+++ MataKuliah.java
@@ -11,9 +11,29 @@ public class MataKuliah {
         this.sks = sks;
     }
 
+    MataKuliah[] listMK = new MataKuliah[3];
+    int idx;
+
+    void tambah(MataKuliah mk) {
+        if (idx < listMK.length) {
+            listMK[idx] = mk;
+            idx++;
+        }
+        else {
+            System.out.println("Data sudah penuh");
+        }
+    }
+
     void tampilMatakuliah() {
         System.out.println("Kode Mata Kuliah : " + this.kodeMK);
         System.out.println("Nama Mata Kuliah : " + this.namaMK);
         System.out.println("SKS              : " + this.sks);
+    }
+
+    void tampilDataMK() {
+        for (MataKuliah mk : listMK) {
+            mk.tampilMatakuliah();
+            System.out.println();
+        }
     }
 }
